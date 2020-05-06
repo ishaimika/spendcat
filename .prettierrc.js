9b7273e86e2bbf092ac987ff48a1d2a085fdef2c
Simon Mika <simon@mika.se>
2020-05-06 20:09:37 +0200

    Initial project

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: false,
+  trailingComma: "all",
+  singleQuote: false,
+  quoteProps: "as-needed",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  printWidth: 120,
+  tabWidth: 2,
+  arrowParens: "always",
+  endOfLine: "lf",
+}
